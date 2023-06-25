523ac5dbb7b71da2d49c5812d44311b1405f45cf
Resmart <90556532+Resmart1@users.noreply.github.com>
2023-06-25 23:06:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
 	cors: {
-		origin: ["https://resmart1.github.io/chatmore", "https://resmart1.github.io"],
+		origin: "*",
 		methods: ["GET", "POST"],
 	}
 });
