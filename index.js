934ddbf2857277268994b7d94a5efdc2a254387b
Resmart <90556532+Resmart1@users.noreply.github.com>
2023-06-30 17:27:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const messageRoutes = require("./routes/message-routes");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 app.use(express.json());
